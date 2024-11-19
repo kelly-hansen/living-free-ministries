bfcccd4cac605c63566aa2cfbe42943c342b6840
Kelly Hansen <kelly.hansen@hotbsoftware.com>
2024-11-19 11:02:53 -0800

    Contact

--- js/main.js
+++ js/main.js
@@ -4,5 +4,8 @@ $phoneNumber.textContent = '(949) 735-4321';
 const $emailAddress = document.querySelector('.email-address');
 $emailAddress.textContent = 'Sheneau@LivingFreeMinistries.org';
 
+const $deborahEmailAddress = document.querySelector('.deborah-email-address');
+$deborahEmailAddress.textContent = 'Deborah@LivingFreeMinistries.org';
+
 // const $instagram = document.querySelector('.instagram');
 // $instagram.textContent = '@BigosLLC';
